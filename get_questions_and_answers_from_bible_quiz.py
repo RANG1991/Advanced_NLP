02c123df0e3c0da059da46ff49a7d189e4d56caf
Ran <galun.ran@gmail.com>
2023-04-01 19:24:53 +0300

    improving parsing of PDF

--- get_questions_and_answers_from_bible_quiz.py
+++ get_questions_and_answers_from_bible_quiz.py
@@ -1,29 +1,29 @@
 from tika import parser
 import re
 
-
 PATTERN_Q_A_NUMBER = "(\\d+)(\\)|\\.)"
 
 
 def main():
     with open("./QA_bible.txt", "w", encoding="utf-8") as f:
         raw = parser.from_file("english.nationalexam2020-1.pdf")
-        list_questions = re.findall(f"{PATTERN_Q_A_NUMBER}(.*?\\?)(.*?)(?={PATTERN_Q_A_NUMBER})", raw["content"], flags=re.DOTALL)
+        list_questions = re.findall(f"{PATTERN_Q_A_NUMBER}(.*?\\?)(.*?)(?={PATTERN_Q_A_NUMBER})", raw["content"],
+                                    flags=re.DOTALL)
         for qa in list_questions:
             question = qa[2]
             answers = qa[3]
             question_formatted = re.sub("\n+", " ", question)
             question_formatted = re.sub(" +", " ", question_formatted)
             question_formatted = question_formatted.replace("\u200b", "")
-            answers_formatted = re.sub("\n+", " ", answers)
+            answers_formatted = re.sub("\n+", "\n", answers)
             answers_formatted = re.sub(" +", " ", answers_formatted)
-            answers_formatted = re.sub("([a-zA-Z]( )*(\\.|\\)))", "", answers_formatted)
+            answers_formatted = re.sub("([a-zA-Z]( )*(\\.|\\)))", "", answers_formatted).split("\n")
             print(f"question: {question_formatted}")
             f.write(f"question: {question_formatted}\n")
-            answers_formatted = [answer.strip().replace("\u200b", "") for answer in answers_formatted.split('\n')
-                                 if answer.strip() != '']
-            print(f"answers: {answers_formatted}")
-            f.write(f"answers: {answers_formatted}\n")
+            answers_formatted = ",".join(
+                [answer.strip().replace("\u200b", "") for answer in answers_formatted if answer.strip() != ''])
+            print(f"answers: [{answers_formatted}]")
+            f.write(f"answers: [{answers_formatted}]\n")
 
 
 if __name__ == "__main__":
