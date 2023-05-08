89c59c555632295bb794e4bfe5928a2cb35fbddb
Ran <galun.ran@gmail.com>
2023-05-08 09:02:53 +0300

    continue EX1

--- ex1.py
+++ ex1.py
@@ -80,68 +80,88 @@ class Pre_Train_On_SST2(nn.Module):
         return output
 
 
-def prepare_model(model_name):
+def prepare_model_pytorch(model_name):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = Pre_Train_On_SST2(model_name)
     model = model.to(device)
     return model, tokenizer
 
 
-def train_and_validate_using_pytorch(args, dataset_train, dataset_val, model_name):
+def train_and_validate_using_pytorch(args, dataset_train, dataset_val, model, tokenizer):
     loss_func = nn.BCELoss()
-    model, tokenizer = prepare_model(model_name)
     optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
     list_acc = []
+    dataloader_train = DataLoader(dataset_train, batch_size=args.per_device_train_batch_size, shuffle=True)
+    dataloader_val = DataLoader(dataset_val, batch_size=args.per_device_train_batch_size, shuffle=False)
     for _ in range(int(args.num_train_epochs)):
-        dataloader_train = DataLoader(dataset_train, batch_size=args.per_device_train_batch_size, shuffle=True)
         train_epoch(model, tokenizer, dataloader_train, loss_func, optimizer)
-        # for _ in range(int(args.num_val_epochs)):
-        dataloader_val = DataLoader(dataset_val, batch_size=args.per_device_train_batch_size, shuffle=False)
         acc_epoch = val_epoch(model, tokenizer, dataloader_val)
         list_acc.append(acc_epoch.item())
     return list_acc
 
 
-def predict_using_pytorch(model_name, test_dataset):
-    model, tokenizer = prepare_model(model_name)
-    model.eval()
+def predict_using_pytorch(model_name, dict_model_name_to_model_obj_and_best_acc_seed, test_dataset):
+    best_model_obj, best_tokenizer_obj, best_seed = dict_model_name_to_model_obj_and_best_acc_seed[model_name]
+    initialize_seed(best_seed)
+    best_model_obj.eval()
     list_predictions = []
     with torch.no_grad():
         for dict_example in test_dataset:
             X = dict_example["sentence"][:]
-            X = tokenizer(X, max_length=tokenizer.model_max_length, truncation=True, return_tensors='pt')
+            X = best_tokenizer_obj(X, max_length=best_tokenizer_obj.model_max_length,
+                                   truncation=True, return_tensors='pt')
             X = X.to(device)
-            y_hat = model(X)
+            y_hat = best_model_obj(X)
             list_predictions.append((dict_example["sentence"], y_hat.round().item()))
     return list_predictions
 
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
+    predictions = np.argmax(logits, axis=1)
     metric = evaluate.load("accuracy")
     return metric.compute(predictions=predictions, references=labels)
 
 
-def train_and_validate_using_hugging_face(args, dataset_train, dataset_val, model_name, seed):
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-    dataset_train = dataset_train.map(lambda example: tokenizer(example["sentence"],
-                                                                max_length=tokenizer.model_max_length, padding=True,
-                                                                truncation=True, return_tensors='pt'), batched=True)
-    dataset_val = dataset_val.map(lambda example: tokenizer(example["sentence"],
-                                                            max_length=tokenizer.model_max_length, padding=True,
-                                                            truncation=True, return_tensors='pt'), batched=True)
+def prepare_model_hugging_face(model_name, args, dataset_train, dataset_val, seed):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     config = AutoConfig.from_pretrained(model_name)
     config.num_labels = 2
     model = AutoModelForSequenceClassification.from_config(config=config)
     args.evaluation_strategy = "epoch"
+    dataset_train = dataset_train.map(lambda example: tokenizer(example["sentence"],
+                                                                max_length=tokenizer.model_max_length,
+                                                                truncation=True), batched=True).shuffle(seed=seed)
+    dataset_val = dataset_val.map(lambda example: tokenizer(example["sentence"],
+                                                            max_length=tokenizer.model_max_length,
+                                                            truncation=True), batched=True)
     trainer = Trainer(
         model=model,
         args=args,
-        train_dataset=dataset_train.shuffle(seed=seed),
+        train_dataset=dataset_train,
         eval_dataset=dataset_val,
+        tokenizer=tokenizer,
         compute_metrics=compute_metrics)
+    return trainer, tokenizer
+
+
+def train_and_validate_using_hugging_face(trainer):
     trainer.train()
+    metrics = trainer.evaluate()
+    return [metrics["eval_accuracy"]]
+
+
+def predict_using_hugging_face(model_name, dict_model_name_to_model_obj_and_best_acc_seed, test_dataset):
+    best_trainer, best_tokenizer, best_seed = dict_model_name_to_model_obj_and_best_acc_seed[model_name]
+    initialize_seed(best_seed)
+    test_dataset = test_dataset.map(lambda example: best_tokenizer(example["sentence"],
+                                                                   max_length=best_tokenizer.model_max_length,
+                                                                   truncation=True),
+                                    batched=True).shuffle(seed=best_seed)
+    predictions = best_trainer.predict(test_dataset)
+    preds = np.argmax(predictions.predictions, axis=-1)
+    examples_with_preds = zip(test_dataset, preds)
+    return examples_with_preds
 
 
 def create_res_file(dict_model_name_to_acc_list, training_time, prediction_time):
@@ -181,24 +201,27 @@ def main():
                                                                command_args.num_samples_validation,
                                                                command_args.num_samples_test)
     dict_model_name_to_acc_list = {}
-    dict_model_name_to_best_acc_seed = {}
+    dict_model_name_to_model_obj_and_best_acc_seed = {}
     start_training_time = time()
     for model_name in models_names:
         best_mean_seed_acc = None
         best_seed = None
+        best_trainer_obj = None
+        best_tokenizer_obj = None
         for seed in range(command_args.num_seeds):
             initialize_seed(seed)
             training_args = TrainingArguments("working_dir")
-            list_acc = train_and_validate_using_pytorch(training_args, dataset_train, dataset_val, model_name)
+            trainer, tokenizer = prepare_model_hugging_face(model_name, training_args, dataset_train, dataset_val, seed)
+            list_acc = train_and_validate_using_hugging_face(trainer)
             if model_name not in dict_model_name_to_acc_list.keys():
                 dict_model_name_to_acc_list[model_name] = []
             dict_model_name_to_acc_list[model_name].extend(list_acc)
             if best_mean_seed_acc is None or best_mean_seed_acc < np.mean(list_acc):
                 best_mean_seed_acc = np.mean(list_acc)
                 best_seed = seed
-        dict_model_name_to_best_acc_seed[model_name] = best_seed
-        # for model_name in models_names:
-        #     train_and_validate_using_hugging_face(training_args, dataset_train, dataset_val, model_name, seed)
+                best_trainer_obj = trainer
+                best_tokenizer_obj = tokenizer
+        dict_model_name_to_model_obj_and_best_acc_seed[model_name] = (best_trainer_obj, best_tokenizer_obj, best_seed)
     dur_training_time = time() - start_training_time
     start_prediction_time = time()
     model_name_with_max_acc = None
@@ -209,7 +232,9 @@ def main():
         if best_acc is None or best_acc < mean_acc:
             model_name_with_max_acc = model_name
             best_acc = mean_acc
-    list_predictions = predict_using_pytorch(model_name_with_max_acc, dataset_test)
+    list_predictions = predict_using_hugging_face(model_name_with_max_acc,
+                                                  dict_model_name_to_model_obj_and_best_acc_seed,
+                                                  dataset_test)
     dur_prediction_time = time() - start_prediction_time
     create_res_file(dict_model_name_to_acc_list, dur_training_time, dur_prediction_time)
     create_predictions_file(list_predictions)
