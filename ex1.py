8d440f63ec001b81bd59dba0f28162ea01bfa138
Ran <galun.ran@gmail.com>
2023-05-21 17:06:50 +0300

    commenting wandb

--- ex1.py
+++ ex1.py
@@ -10,7 +10,7 @@ import argparse
 from time import time
 # import wandb
 
-use_wandb = False
+# use_wandb = False
 models_names = ["bert-base-uncased", "roberta-base", "google/electra-base-generator"]
 device = ("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -46,11 +46,11 @@ def prepare_model_hugging_face(model_name, args, dataset_train, dataset_val, see
     model = AutoModelForSequenceClassification.from_pretrained(model_name, config=config)
     args.evaluation_strategy = "epoch"
     args.save_strategy = "no"
-    if use_wandb:
-        args.report_to = ["wandb"]
-        args.run_name = model_name
-    else:
-        args.report_to = []
+    # if use_wandb:
+    #     args.report_to = ["wandb"]
+    #     args.run_name = model_name
+    # else:
+    args.report_to = []
     dataset_train = dataset_train.map(lambda example: tokenizer(example["sentence"],
                                                                 max_length=tokenizer.model_max_length,
                                                                 truncation=True), batched=True).shuffle(seed=seed)
@@ -147,8 +147,8 @@ def main():
                 best_trainer_obj = trainer
                 best_tokenizer_obj = tokenizer
         dict_model_name_to_model_obj_and_best_acc_seed[model_name] = (best_trainer_obj, best_tokenizer_obj, best_seed)
-        if use_wandb:
-            wandb.finish()
+        # if use_wandb:
+        #     wandb.finish()
     dur_training_time = time() - start_training_time
     start_prediction_time = time()
     model_name_with_max_acc = None
